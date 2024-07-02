10109c469243954b73b70244132a36ccb6c5943b
allen <allenthomas204@gmail.com>
2024-07-02 20:32:19 +0530

    added leetcode-217 contains-duplicate

---
+++ contains_duplicate.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+
+        HashSet<Integer> hashset = new HashSet<Integer>();
+
+        for(int i=0;i<nums.length;i++){
+            if(hashset.contains(nums[i])){
+                return true;
+            }
+            else{
+                hashset.add(nums[i]);
+            }
+        }
+        return false;
+        
+ 
+    }
+}
