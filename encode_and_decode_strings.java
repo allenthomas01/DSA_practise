2f2915a0f7bc850df9dccf4c0f734e2497e06922
allen <allenthomas204@gmail.com>
2024-09-18 14:44:37 +0000

    leetcode-271 String Encode and Decode
    Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
    Please implement encode and decode

---
+++ encode_and_decode_strings.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder encodedstring = new StringBuilder();
+        for(String str:strs){
+            encodedstring.append(str.length());
+            encodedstring.append('#');
+            encodedstring.append(str);
+        }
+        return encodedstring.toString();
+
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+            int j = str.indexOf("#", i);
+            int length = Integer.parseInt(str.substring(i, j));
+            i = j + 1;
+            res.add(str.substring(i, i + length));
+            i += length;
+        }
+        return res;
+
+    }
+}
