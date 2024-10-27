9ae409a5c52ac377095b01812b82d703c9fc15dc
allen <allenthomas204@gmail.com>
2024-10-27 06:58:44 +0000

    modified leetcode20.py

--- python/leetcode20.py
+++ python/leetcode20.py
@@ -1,15 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        Map = {")": "(", "]": "[", "}": "{"}
-        stack = []
-
-        for c in s:
-            if c not in Map:
-                stack.append(c)
-                continue
-            if not stack or stack[-1] != Map[c]:
-                return False
-            stack.pop()
-
-        return not stack
+        stack=[]
+        ch=''
+        for i in range(len(s)):
+            if s[i]=="(" or s[i]=="[" or s[i]=="{":
+                stack.append(s[i])
+            else:
+                if not stack:
+                    return False
+                if stack[-1]=="{" and s[i]=="}" or\
+                   stack[-1]=="(" and s[i]==")" or\
+                   stack[-1]=="[" and s[i]=="]":
+                       stack.pop()
+                       continue
+                else:
+                    return False
+            
+        if stack:
+            return False
+        else:
+            return True
     
